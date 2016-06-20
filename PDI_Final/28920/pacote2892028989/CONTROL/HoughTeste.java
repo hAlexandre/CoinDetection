325400406d1160729b43d29d323e64694006816f
Alexandre <alexandreoliveiraa1@hotmail.com>
2016-06-20 14:57:50 -0300

    commit final

--- PDI_Final/28920/pacote2892028989/CONTROL/HoughTeste.java
+++ PDI_Final/28920/pacote2892028989/CONTROL/HoughTeste.java
@@ -2,7 +2,13 @@ package pacote2892028989.CONTROL;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import pacote2892028989.MODEL.Ponto;
 
@@ -10,7 +16,7 @@ public class HoughTeste {
 	
 	ArrayList<Ponto> pontos, proximos;
 	Boolean[][] visitados;
-	int nCol, nLin, maX, meX, maY, meY;
+	int nCol, nLin, maX, meX, maY, meY, moedas;
 	char[][] imagem;
 	Graphics g;
 	public HoughTeste (char[][] imagem, int nLin, int nCol, Graphics g)
@@ -35,10 +41,17 @@ public class HoughTeste {
 			}
 		}
 		
+		moedas = 0;
+		BufferedWriter bw = null;
+		
+		
+		File arquivo = new File("arquivo2.txt");
+		
 		for(int cont = 0 ; cont < pontos.size() ; cont++)
 		{
 			if(!visitados[pontos.get(cont).getX()][pontos.get(cont).getY()])
 			{
+				moedas++;
 				proximos = new ArrayList<>();
 				proximos.add(pontos.get(cont));
 				
@@ -54,52 +67,86 @@ public class HoughTeste {
 					{
 						
 					}
-				}
+				}				
+				
 				for(int x = meX ; x <= maX ; x++)
 				{
 					for(int y = meY ; y<= maY ; y++)
 					{
+						
+						
+					
+									
+						
 						if(imagem[x][y]==255)
-						{
-							
+						{							
 							double r = ( (maX-meX)/2 + (maY-meY)/2 ) / 2;
 							double dx = Math.pow(x-(meX+(maX-meX)/2), 2);
 							double dy = Math.pow(y-(meY+(maY-meY)/2), 2);					
 							if ( (Math.sqrt (dx+dy) <= r) )
 							{
-								visitados[x][y] = true;										
 //								g.drawLine(x, y, x, y);
+								visitados[x][y] = true;										//								
 							}
-						}
-						
+						}		
 					}
+					
 				}
+				
+				g.setColor(Color.GREEN);
 				g.drawLine(maX, maY, maX, meY);	
 				g.drawLine(meX, maY, meX, meY);
 				g.drawLine(meX, meY, maX, meY);
 				g.drawLine(meX, maY, maX, maY);
-				g.drawOval(meX+(maX-meX)/2, meY+(maY-meY)/2, 2, 2);
+//				contaPixel();
+				
 			
-			}
-			
-			
+			}						
 		}
+		System.out.println(moedas+" moedas");
 		
 		
+	}
+	
+	private void contaPixel()
+	{
+		int x = (maX - meX) /2 ;
+		int y = (maY - meY) /2;
+				
+		int offset = (int)(x*0.7);
 		
+		x+= meX;
+		y+= meY;
+		
+//		g.drawLine(x,y-offset,x+2*offset,y-offset);
+//		g.drawLine(x, y+offset, x, y-offset);
+//		g.drawLine(x+2*offset,y-offset,x+2*offset,y+offset);
+//		g.drawLine(x+2*offset, y+offset, x, y+offset);
+//		
+//		
+//		for(int i = x-offset ; i <= (x+0.5*offset) ; i++)
+//		{
+//			for(int j = (int) (y-offset*1.2) ; j <= (y+0.42*offset) ;j++)
+//			{
+//				
+//				g.drawLine(i, j, i, j);	
+//			}
+//		}
+
+
 	}
 
 	private void verifica(Ponto p)
 	{		
 		int xp = p.getX();
 		int yp = p .getY();
-			for(int i = -2 ; i <= 2 ; i++)
+			for(int i = -4 ; i <= 4 ; i++)
 			{
-				for(int j = -2 ; j <= 2 ; j++)
+				for(int j = -4 ; j <= 4 ; j++)
 				{
 					int x = xp + i;
 					int y = yp + j;
-					if( (x<0) || (y<0) || (x>=nCol) || (y>=nCol)  ) 											
+					if( (x<0) || (y<0) || (x>=nCol) || (y>=nLin)  ) 											
 						continue;											
 					
 					
@@ -110,6 +157,8 @@ public class HoughTeste {
 						if(y<meY) meY = y;
 						if(y>maY) maY = y;
 						visitados[x][y]=true;
+						g.setColor(Color.green);
+//						g.drawLine(x, y, x, y);
 						proximos.add(new Ponto(x,y));						
 					}										
 				}
